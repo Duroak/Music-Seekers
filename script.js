9c74c2185a0b08622b5e6ae2aaca2943cf4f9c74
Duro Akintunde <duroak@yahoo.com>
2019-10-11 15:43:02 -0500

    styled the app

--- script.js
+++ script.js
@@ -8,29 +8,33 @@ function formatQueryParams(params) {
     return queryItems.join('&');
 }
 
+//displays in the artist results section of the html the title,
+//the thumbnail, and the Biography
 function displayArtists(responseJson) {
     console.log(responseJson);
     $('#js-artist-results').empty();
     for (let i=0; i < responseJson.artists.length; i++) {
         $('#js-artist-results').append(
-            `<h1>${responseJson.artists[i].strArtist}<h1>
+            `<p id="name">${responseJson.artists[i].strArtist}<p>
             <img src="${responseJson.artists[i].strArtistThumb}">
             <p>${responseJson.artists[i].strBiographyEN}</p>`
         )};
     $('#js-results-page').removeClass('hidden');
 } 
 
+//displays the response from youtube api (the videos) in a list
+//unorder list that also includes a title for the video 
 function displayYoutube(responseJson) {
     console.log(responseJson);
     $('#js-videos').empty();
     for (let i=0; i < responseJson.items.length; i++) {
         $('#js-videos').append(
-            `<li><h3>${responseJson.items[i].snippet.title}</h3>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
+            `<li><iframe id="ytplayer" type="text/html" width="640" height="360 fs="1" frameborder="0"
             src="https://www.youtube-nocookie.com/embed/${responseJson.items[i].id.videoId}?autoplay=0 frameborder="0"></iframe><li>`            
         )};   
 }
 
+//fetching the artist Picture and description
 function getArtist(query) {
     const params = {
         s: query,  
@@ -51,13 +55,14 @@ function getArtist(query) {
         });
 }
 
+//fetching the arist videos from youtube api
 function getYoutubeVideo(query) {
     const params = {
         key: youtubeApi,
         part: 'snippet', 
         q: query,
         type: 'videos', 
-        maxResult: '4'
+        maxResults: '6'
     }
 
     const queryString2 = formatQueryParams(params)
@@ -76,13 +81,21 @@ function getYoutubeVideo(query) {
         }); 
 }
 
+function scroll() {
+    $('html, body').animate({
+        scrollTop: $("#js-results-page").offset().top
+    }, 1000);
+}
+
+//searching the name of artist or group and scrolls to the section of artist bio
 function submitform() {
-    $('form').submit(event => {
+    $('form').on('submit', function() {
         event.preventDefault();
         const searchArtist = $('#js-search-artist').val();
         getArtist(searchArtist);
         getYoutubeVideo(searchArtist);
         $('#js-error-msg').empty();
+        scroll();
     });
 }
 
